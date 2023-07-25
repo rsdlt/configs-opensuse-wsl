5c3b7612cbffae40ac7cb0e81b86436cdb092c8e
Rodrigo Santiago <rsdlt@pm.me>
2023-07-25 10:10:16 -0400

    fsh update

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     if not set -q TMUX
-        set -g TMUX tmux new-session -d -s base
+        set -g TMUX tmux new-session -d -s base -c "$HOME/"
         eval $TMUX
         tmux attach-session -d -t base
     end
@@ -10,23 +10,19 @@ end
 # add Rust to path 
 set PATH $HOME/.cargo/bin $PATH
 
-# function fish_greeting
-#     echo $USER@$HOSTNAME - (set_color yellow; date '+%T // %A, %d %B %Y'; set_color normal)
-# end
+# no greeting
+set fish_greeting
 
 set -Ux TERM xterm-256color
 set -Ux EDITOR nvim
 
-alias e "exit"
+alias ex "exit"
 alias cl "clear"
-alias cr "cargo run -q"
-alias ct "cargo test"
-alias ctnc "cargo test -- --nocapture"
-alias cb "cargo build"
-alias cch "cargo check"
 alias cat "bat"
 alias neovim "nvim"
 alias nv "nvim"
+alias cdh "cd ~"
+alias cdp "cd ~/projects/"
 
 
 starship init fish | source
